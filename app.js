195922f096df2a72104f5b9d57490e8829507f7b
Jayed Rehan <138893847+Astro-Jr@users.noreply.github.com>
2023-10-21 12:16:05 +0600

    Added empty checker in app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // <-- The function
 
 function CountRangeOccurance(list, start, end){
+// <-- Check if the list is empty
+    if(list.length === 0 && list.length === null){
+      return `The ${list} is empty`;
+    }
+  
   // <-- Variables
- 
     let count = 0;
 
   // <-- Main proccess
